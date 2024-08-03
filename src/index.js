3ff94e073d8adb5f87528a2f07c23e3a14b27c49
bycarlablau <lopezseguracarla98@gmail.com>
2024-08-03 13:10:19 +0200

    Updated forecast with real data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityValueElement = document.querySelector("#humidity-value");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
-    
-    cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = formatDate(date);
-    descriptionElement.innerHTML = response.data.condition.description;
-    humidityValueElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="tiempo-app-icon" />`;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityValueElement = document.querySelector("#humidity-value");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-    getForecast(response.data.city);
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityValueElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="tiempo-app-icon" />`;
 
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -43,45 +42,58 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-    let apiKey = "7a58ae3be13fa849e3795c2toa1750fb";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "7a58ae3be13fa849e3795c2toa1750fb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    
-    searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+  
+  
 function getForecast(city) {
-    let apiKey = "7a58ae3be13fa849e3795c2toa1750fb";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-    axios(apiUrl).then(displayForecast);
+  let apiKey = "7a58ae3be13fa849e3795c2toa1750fb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-    console.log(response.data);
+  console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5){
     forecastHtml =
       forecastHtml +
       `
       <div class="tiempo-app-forecast-day">
-        <div class="tiempo-app-forecast-date">${day}</div>
-        <div class="tiempo-app-forecast-icon">🌤️</div>
+        <div class="tiempo-app-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="tiempo-app-forecast-icon" />
         <div class="tiempo-app-forecast-temperatures">
           <div class="tiempo-app-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="tiempo-app-forecast-temperature">9º</div>
+          <div class="tiempo-app-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
         </div>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -92,5 +104,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Amsterdam");
-
-
